2036bcc28426974ca0f9adf7b4be3ed3b2cc4428
Ibullayev Zhanibek <ibullayevzhanibek@gmail.com>
2024-06-05 15:31:41 +0500

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Zhanibek Ibullayev on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
